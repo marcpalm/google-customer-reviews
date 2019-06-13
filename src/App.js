bf24d685e207a21b02846e627e8e16727b2ae18c
Marc Palm <palm@testless.io>
2019-06-13 17:35:39 +0200

    Fix

--- src/App.js
+++ src/App.js
@@ -4,22 +4,16 @@ import './App.css';
 
 import GoogleCustomerReview from './GoogleCustomerReview'
 
-class App extends React.Component {
-  state = {
-    email: 'palmmarc85@gmail.com',
-    orderId: 1,
-    deliveryCountry: 'DE'
-  }
+const onClick  = (_) => GoogleCustomerReview.initialize().then(render => render({
+  email: 'palmmarc85@gmail.com',
+  orderId: '2',
+  deliveryCountry: 'DE'
+ }), e => console.error(e))
 
-  render = () => (
-    <div className="App">
-        <form>
-            <input type="button" id="submit" onClick={
-              (_) => GoogleCustomerReview.initialize().then(_ => _.render({ ...this.state }), console.error )
-            } />
-        </form>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+        <input type="button" id="submit" onClick={onClick} />
+  </div>
+)
 
 export default App;
